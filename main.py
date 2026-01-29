d97756bade088c606c74c761ce999f24edcf43b1
Sagar Patil <sagardev519@gmail.com>
2026-01-29 23:26:59 +0530

    post method - using basemodel (pydantic )

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Importing FastAPI 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # Creating an instance of FastAPI
 app = FastAPI()
@@ -27,8 +28,6 @@ def read_items(item_id: int):
         return {"item_id": item_id}
 
 
-
-
 #Query parameter - It is used for send dynamic values in the URL
 @app.get("/emp/{emp_dept}")
 def read_emp_dept(emp_dept: str, dept_id: int):
@@ -38,3 +37,16 @@ def read_emp_dept(emp_dept: str, dept_id: int):
 @app.get("/shopping/{shopping_item}")
 def read_shopping_item(shopping_item: str, shopping_company: str | None = None):
     return {"Message": f"Shopping item - {shopping_item} - Company - {shopping_company}"}
+
+
+#post method - It is used for create new resources 
+# In this example, we are creating a new student resource
+
+#we use pydantic model to define the structure of the request body
+class Student(BaseModel):
+     name:str
+     age:int
+
+@app.post("/student")
+def read_student(student: Student):
+    return {"Message": f"Student - {student.name} - Age - {student.age}"}
